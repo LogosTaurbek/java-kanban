13602dd065ce0997d536cd8e51fc7b1bf02556c3
m.taurbek <m.taurbek@alem.kz>
2025-05-23 20:56:31 +0500

    add fixes on formating variables name

--- src/Main.java
+++ src/Main.java
@@ -14,25 +14,25 @@ public class Main {
         TaskManager taskManager = taskManagerUtil.getDefault();
 
         Task task1 = new Task("Задача 1", "Описание задачи 1.");
-        int task1_id = taskManager.createTask(task1);
+        int task1Id = taskManager.createTask(task1);
         Task task2 = new Task("Задача 2", "Описание задачи 2.");
-        int task2_id = taskManager.createTask(task2);
+        int task2Id = taskManager.createTask(task2);
 
         /*Create epic with 2 subtasks */
 
         Epic epic1 = new Epic("Эпик 1", "Описание эпика 1");
-        int epic1_id = taskManager.createEpic(epic1);
-        Subtask subtask_1_1 = new Subtask("Подзадача 1-1", "Описание подзадачи 1-1.", epic1_id);
-        int subtask_1_1_id = taskManager.createSubtask(subtask_1_1);
-        Subtask subtask_1_2 = new Subtask("Подзадача 1-2", "Описание подзадачи 1-2.", epic1_id);
-        int subtask_1_2_id = taskManager.createSubtask(subtask_1_2);
+        int epic1Id = taskManager.createEpic(epic1);
+        Subtask subtask11 = new Subtask("Подзадача 1-1", "Описание подзадачи 1-1.", epic1Id);
+        int subtask11Id = taskManager.createSubtask(subtask11);
+        Subtask subtask12 = new Subtask("Подзадача 1-2", "Описание подзадачи 1-2.", epic1Id);
+        int subtask12Id = taskManager.createSubtask(subtask12);
 
         /* Create epic with 1 subtask */
 
         Epic epic2 = new Epic("Эпик 2", "Описание эпика 2");
-        int epic2_id = taskManager.createEpic(epic2);
-        Subtask subtask_2_1 = new Subtask("Подзадача 2-1", "Описание подзадачи 2-1.", epic2_id);
-        int subtask_2_1_id = taskManager.createSubtask(subtask_2_1);
+        int epic2Id = taskManager.createEpic(epic2);
+        Subtask subtask21 = new Subtask("Подзадача 2-1", "Описание подзадачи 2-1.", epic2Id);
+        int subtask21Id = taskManager.createSubtask(subtask21);
 
         System.out.println(taskManager.getTasks());
         System.out.println(taskManager.getEpics());
