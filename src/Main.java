04259156e3051f100eaaf90aed25139dcd464c3b
LogosTaurbek <taurbek@yandex.ru>
2025-08-10 21:20:52 +0600

    add http task server

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,12 @@
-import model.Epic;
+import httpserver.HttpTaskServer;
 import model.Managers;
-import model.Subtask;
-import model.Task;
-import service.TaskManager;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Трекер задач");
-
-        /* Create 2 task */
         Managers taskManagerUtil = new Managers();
-        //TaskManager taskManager = taskManagerUtil.getDefault();
-
-        TaskManager taskManager = taskManagerUtil.getFileBackedTaskManager("tasks.csv");
-
-        Task task1 = new Task("Задача 1", "Описание задачи 1.");
-        int task1Id = taskManager.createTask(task1);
-        Task task2 = new Task("Задача 2", "Описание задачи 2.");
-        int task2Id = taskManager.createTask(task2);
-
-        /*Create epic with 2 subtasks */
-
-        Epic epic1 = new Epic("Эпик 1", "Описание эпика 1");
-        int epic1Id = taskManager.createEpic(epic1);
-        Subtask subtask11 = new Subtask("Подзадача 1-1", "Описание подзадачи 1-1.", epic1Id);
-        int subtask11Id = taskManager.createSubtask(subtask11);
-        Subtask subtask12 = new Subtask("Подзадача 1-2", "Описание подзадачи 1-2.", epic1Id);
-        int subtask12Id = taskManager.createSubtask(subtask12);
-
-        /* Create epic with 1 subtask */
-
-        Epic epic2 = new Epic("Эпик 2", "Описание эпика 2");
-        int epic2Id = taskManager.createEpic(epic2);
-        Subtask subtask21 = new Subtask("Подзадача 2-1", "Описание подзадачи 2-1.", epic2Id);
-        int subtask21Id = taskManager.createSubtask(subtask21);
-
-        System.out.println(taskManager.getTasks());
-        System.out.println(taskManager.getEpics());
-        System.out.println(taskManager.getSubtasks());
-        printHistory(taskManager);
-
-    }
-
-    private static void printHistory(TaskManager taskManager) {
-        System.out.println("История просмотра задач:");
-        for (Task historyItem : taskManager.getHistory()) {
-            System.out.println(historyItem);
-        }
+        service.TaskManager taskManager = taskManagerUtil.getDefault();
+        HttpTaskServer httpTaskServer = new HttpTaskServer(taskManager);
+        httpTaskServer.start();
     }
 }
---
+++ test/httpservertests/HttpTaskManagerPrioritizedTest.java
@@ -0,0 +1,62 @@
+package httpservertests;
+
+import httpserver.HttpTaskServer;
+import model.Epic;
+import model.Subtask;
+import model.Task;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.InMemoryTaskManager;
+import service.TaskManager;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpTaskManagerPrioritizedTest {
+    TaskManager manager = new InMemoryTaskManager();
+    HttpTaskServer taskServer = new HttpTaskServer(manager);
+
+    public HttpTaskManagerPrioritizedTest() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+        manager.removeAllTasks();
+        manager.removeAllSubtasks();
+        manager.removeAllEpics();
+        taskServer.start();
+    }
+
+    @AfterEach
+    public void shutDown() {
+        taskServer.stop();
+    }
+
+    @Test
+    public void testGetPrioritized() throws IOException, InterruptedException {
+        Task task1 = new Task("task 1", "description 1", LocalDateTime.now(), Duration.ofHours(1));
+        manager.createTask(task1);
+        Epic epic = new Epic("epic", "description");
+        manager.createEpic(epic);
+        int epicId = manager.getEpics().getLast().getId();
+        Subtask subtask = new Subtask("subtask", "description", LocalDateTime.of(2025, 1, 1, 12, 34), Duration.ofHours(1), epicId);
+        manager.createSubtask(subtask);
+        HttpResponse<String> response;
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            URI url = URI.create("http://localhost:8080/prioritized");
+            HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        }
+        assertEquals(200, response.statusCode());
+        String expectedString = manager.getPrioritizedTasks().toString();
+        assertEquals(expectedString, response.body());
+    }
+}
